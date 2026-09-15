c0edc83bff3be42b0a5eb57e810cafffd7124e2d
DFIR-commits <curtodon6@protonmail.com>
2026-09-15 11:38:42 -0500

    Enhance api_test.py with vCenter connection example
    Added a code snippet for connecting to vCenter and testing snapshot creation.

--- api_test.py
+++ api_test.py
@@ -7,3 +7,18 @@ t = vm.CreateSnapshot_Task(name='test', description='test', memory=True, quiesce
 while t.info.state not in ['success','error']: time.sleep(1)
 print(t.info.state, t.info.error.msg if t.info.state=='error' else '')
 "
+
+
+
+
+python3 -c "
+from pyVim.connect import SmartConnect
+import ssl
+ctx = ssl.create_default_context(cafile='/opt/vcenter-watcher/vcenter-chain.pem')
+si = SmartConnect(host='vc.net', user='alesher', pwd='YOUR_ACTUAL_PASSWORD_HERE', sslContext=ctx)
+print(repr(si._stub.cookie))
+"
+
+curl -k -H 'Cookie: vmware_soap_session="f7c009fb-7dc0-1f38-1d61-3a44e27886c1"' \
+  "https://vc.net/folder/<vmname>/<filename>?dcPath=localhost&dsName=datastore" \
+  -o /tmp/manual-test.vmsn -v
